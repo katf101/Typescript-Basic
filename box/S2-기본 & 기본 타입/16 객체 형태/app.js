3eb75441e31e44c7cafd2612e8b224dd3665974d
lee <vawav11@gmail.com>
2022-05-25 23:34:18 +0900

    re

---
+++ box/S2-기본 & 기본 타입/16 객체 형태/app.js
@@ -0,0 +1,5 @@
+var person = {
+    name: "katf101",
+    age: 30
+};
+console.log(person.name);
---
+++ box/S2-기본 & 기본 타입/16 객체 형태/app.ts
@@ -0,0 +1,16 @@
+// const person = {
+//   name: "katf101",
+//   age: 30,
+// };
+
+// console.log(person);
+
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "katf101",
+  age: 30,
+};
+
+console.log(person.name);
